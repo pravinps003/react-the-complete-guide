b39a3f4e5230b641e26e81a3b30cfee37d16cff4
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-11 04:43:32 +0530

    created a class Human

--- code/04-understanding-classes/app.js
+++ code/04-understanding-classes/app.js
@@ -1,3 +1,13 @@
+class Human {
+  constructor() {
+    this.gender = 'male';
+  }
+
+  printGender() {
+    console.log(this.gender);
+  }
+}
+
 class Person {
   constructor() {
     this.name = 'Pravin';
