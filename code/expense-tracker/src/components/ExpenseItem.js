8c04340900319b9a12e0f397dfffe2744e7c2e2e
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-11 06:50:34 +0530

    added more complex code
    -  wrapped the html code in a single parent div
    - wrapped with parentheses for better reading

--- code/expense-tracker/src/components/ExpenseItem.js
+++ code/expense-tracker/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>April 11th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$299.98</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
