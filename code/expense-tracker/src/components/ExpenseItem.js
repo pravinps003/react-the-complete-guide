e4403aa6a8978e329e13b4ef9f79efd0a9da7196
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-11 07:02:30 +0530

    rendered dynamic data using expressions in JSX

--- code/expense-tracker/src/components/ExpenseItem.js
+++ code/expense-tracker/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 3, 11);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 299.98;
+
   return (
     <div className="expense-item">
-      <div>April 11th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$299.98</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
