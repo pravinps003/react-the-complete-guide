72ca8e238985e337e5c73b357ee83fabf3d449fa
Pravin Shenoy <21051175+pravinps003@users.noreply.github.com>
2023-04-13 04:55:55 +0530

    managed list of users using state
    - used lifting the state up concept

--- code/user-tracker/src/App.js
+++ code/user-tracker/src/App.js
@@ -3,10 +3,18 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name, age, id: Math.random().toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
